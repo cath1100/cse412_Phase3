538cfb9793479d66d7ba0c183f6595909bbf3b5a
cath1100 <72418866+cath1100@users.noreply.github.com>
2023-04-09 16:26:11 -0700

    start of the connection

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 from flask import Flask, render_template, request
+import mysql.connector
+from mysql.connector import Error
+
+def create_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(host=host_name,user=user_name,password=user_password)
+        print("Connection to MYSQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+
+
+connection= create_connection('http://127.0.0.1', 'root', 'secret')
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
+app.config
 
 
 @app.route('/')
